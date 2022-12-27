48b6ffae91406ae5356f2046aa3a273aa04cdc10
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-12-27 20:16:16 +0000

    Added solution with regex

--- vowelCount.js
+++ vowelCount.js
@@ -19,3 +19,17 @@ function getCount(str) {
   Loop through the provided string
   IF letter from the string is included in the vowel array
   THEN increase the vowel count by one*/
+
+//ALTERNATIVE SOLUTION
+
+function getCount(str) {
+    return ((str || '').match(/[aeiou]/ig)||[]).length;
+  }
+
+/*The || after for str after return prevents break given null param
+'ig' are regexp flags, i is case insensitive and g is the global flag
+if 'str.match(/[aeiou]/ig)' returns a value then that value is used and
+'||[]' is essentially ignored...if it returns null then '[]' empty array
+is returned
+
+*/
