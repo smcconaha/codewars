a7a60ca7d14b8408fb001410e1ec413c3711788c
smcconaha <110992120+smcconaha@users.noreply.github.com>
2023-01-20 08:50:40 -0500

    Create getMiddleChar.js
    added kata solution to Get the Middle Character

---
+++ getMiddleChar.js
@@ -0,0 +1,7 @@
+/*You are going to be given a word. Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.*/
+
+function getMiddle(s) {
+  let middle = s.length / 2
+  return (s.length % 2 === 0) ? s[middle-1]+s[middle] : s[Math.floor(middle)]
+}
