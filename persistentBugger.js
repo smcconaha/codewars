b9baad9274c9cf259e504ff0fd6e49423f62bea6
smcconaha <steve.mcconaha@student.awesomeinc.org>
2023-02-10 15:39:50 +0000

    Added alt solution

--- persistentBugger.js
+++ persistentBugger.js
@@ -17,3 +17,18 @@ function persistence(num) {
     }
     return count
   }
+
+//EASIER TO READ SOLUTION
+
+function persistence(num) {
+    var times = 0;
+    
+    num = num.toString();
+    
+    while (num.length > 1) {
+      times++;
+      num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+    }
+    
+    return times;
+ }
